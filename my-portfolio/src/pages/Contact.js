d8cbd1f98fd65c2e2dbf099dc4edea45159e6500
able24 <egbeleabel@yahoo.com>
2023-02-18 00:48:05 +0000

    made form sent as email to retrieve messages via web3forms

--- my-portfolio/src/pages/Contact.js
+++ my-portfolio/src/pages/Contact.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -10,12 +10,15 @@ import './Contact.css';
 
 function Contact() {
 
+  const [success, setSuccess] = useState(false);
+
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
     email: '',
     phone: '',
-    message: ''
+    message: '',
+    access_key: 'e60801c3-6aac-4656-8f05-556b65a184a0'
   })
 
   const handleChange = (event) => {
@@ -27,33 +30,72 @@ function Contact() {
 
   }
 
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
 
-  event.preventDefault();
+    event.preventDefault();
 
-  console.log(formData);
 
-}
+    const data = JSON.stringify(formData);
+
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: data
+    })
+      .then(res => res.json())
+      .then(data => {
+        setSuccess(true);
+        setFormData({
+          ...formData,
+          firstName: '',
+          lastName: '',
+          email: '',
+          phone: '',
+          message: '',
+        })
+
+        setTimeout(() => {
+          setSuccess(false);
+        }, 3000);
+      })
+      .catch(err => console.log(err));
+
+  };
 
   return (
     <>
       <Header />
 
-      <Form onSubmit = {handleSubmit}>
+      <Form onSubmit={handleSubmit}>
         <h1>Contact Me</h1>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>First Name</Form.Label>
-          <Form.Control name='firstName' onChange = {handleChange} type="text" placeholder="Enter your first name" />
+          <Form.Control name='firstName'
+            value={formData.firstName}
+            onChange={handleChange}
+            type="text"
+            placeholder="Enter your first name" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Last Name</Form.Label>
-          <Form.Control name='lastName' onChange = {handleChange} type="text" placeholder="Enter your last name" />
+          <Form.Control name='lastName'
+            value={formData.lastName}
+            onChange={handleChange}
+            type="text"
+            placeholder="Enter your last name" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control name='email' onChange = {handleChange} type="email" placeholder="Enter your email" />
+          <Form.Control name='email'
+            value={formData.email}
+            onChange={handleChange}
+            type="email"
+            placeholder="Enter your email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -61,18 +103,27 @@ const handleSubmit = (event) => {
 
         <Form.Group className="mb-3" controlId="formBasicPhone">
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control name='phone' onChange = {handleChange} type="tel" placeholder="123-45-678" />
+          <Form.Control name='phone'
+            value={formData.phone}
+            onChange={handleChange}
+            type="tel"
+            placeholder="123-45-678" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Message</Form.Label>
-        <Form.Control name='message' onChange = {handleChange} as="textarea" rows={4} />
-      </Form.Group>
+          <Form.Label>Message</Form.Label>
+          <Form.Control name='message'
+            value={formData.message}
+            onChange={handleChange}
+            as="textarea" rows={4} />
+        </Form.Group>
 
         <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
+
+      {success && <p className='success'>Form Submitted Successfully!</p>}
 
 
       <Footer />
